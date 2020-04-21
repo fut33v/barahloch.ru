7d96fc80e19deaf1ba2abc9d24fc4c889d7007b3
Ilya Fateev <fat32nov@gmail.com>
2020-04-21 12:22:18 +0300

    update styles templates

--- barahloch/templatetags/barahloch_extras.py
+++ barahloch/templatetags/barahloch_extras.py
@@ -10,3 +10,10 @@ def abs_filter(value):
 @register.filter(name='neg')
 def abs_filter(value):
     return -value
+
+
+@register.filter
+def addstr(arg1, arg2):
+    """concatenate arg1 & arg2"""
+    return str(arg1) + str(arg2)
+
