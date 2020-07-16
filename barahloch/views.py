fa9da8018bb9a4371d47f2b8606f25d1478db5a9
Ilya Fateev <fat32nov@gmail.com>
2020-07-16 10:23:22 +0300

    fix

--- barahloch/views.py
+++ barahloch/views.py
@@ -86,7 +86,7 @@ def process_product_buttons(request):
         p.save()
 
         if product_type == 'vkontakte' and state == ProductStateEnum.HIDDEN.name:
-            tasks.delete_good_in_telegram(p.tg_post_id)
+            tasks.delete_good_in_telegram.delay(p.tg_post_id)
 
 
 def process_product_buttons_decorator(func):
